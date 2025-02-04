b91b8cd668b7ca2eca4093d148f13677e286d97d
Erik <rekhel-9@student.ltu.se>
2025-02-04 19:04:04 +0100

    hej

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+
+
 const dotenv = require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.SERVER_PORT;
 
 app.use(express.json());
+
+app.use(cors({ origin :'*'}));
 
 app.use("/api/user", require("./routes/userRoutes"));
 
