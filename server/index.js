f711f5b1d8738779b1065e0e0fa6e82a7c14e0e6
Erik <rekhel-9@student.ltu.se>
2025-02-04 14:11:05 +0100

    start backend

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use("/api/user", require("./routes/userRoutes"));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
