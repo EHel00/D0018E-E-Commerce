eec28bd2eef64ce16648e25918ac7f33b7863be3
rekhel-9 <rekhel-9@student.ltu.se>
2025-02-13 17:17:33 +0100

    asdasd

--- server/query/userQuery.js
+++ server/query/userQuery.js
@@ -1,1 +1,6 @@
-// as
+const UQUERY = {
+    SELECT_USERS: 'SELECT * FROM User;',
+    SELECT_USER: 'SELECT * FROM patients WHERE idUser = ?',
+    CREATE_USER: 'INSERT INTO User(Email, Password, PhoneNumber, FirstName, LastName, Address) VALUES (?, ?, ?, ?, ?, ?)',
+    SELECT_USER_BY_EMAIL: 'SELECT * FROM User WHERE Email = ?',
+};
