91dadc64d065123cb836edf59896c303441a227c
Sebpet <sebpet-9@student.ltu.se>
2025-02-13 17:00:54 +0100

    hasan

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,1 +1,32 @@
-//asd 
+
+
+const db = require('../config/mySqlConfig.js');
+const logger = require('../config/logger.js');
+const query = require('../query/productQuery.js');
+
+
+export const getProducts= async(req, res) => {
+    logger.info('${req.method} ${req.originalUrl}, fetching products');
+    await db.query(PQuery.getAllProducts, (error, results) => {
+        if (!results) {
+            res.status(404).send({message: 'Products not found'});
+
+        } else {
+        res.status(200).send(results,{message: 'Products found'});
+    }
+  });
+};
+
+
+export const getProduct= async(req, res) => {
+    logger.info('${req.method} ${req.originalUrl}, fetching product');
+    await db.query(PQuery.getProductById, (error, results) => {
+        if (!results) {
+            res.status(404).send({message: 'Product not found'});
+
+        } else {
+        res.status(200).send(results,{message: 'Product found'});
+    }
+  });
+};
+module.exports = {getProducts};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,1 +1,9 @@
-//as
+const express = require("express");
+const productController = require("../controller/productController");
+
+const router = express.Router();
+
+
+router.get("/products", productController.getProducts);
+
+module.exports = router;
