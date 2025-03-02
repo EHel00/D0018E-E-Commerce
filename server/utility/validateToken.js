258780de2d5a10f676597372005e78bdc396c6df
Sebpet <sebpet-9@student.ltu.se>
2025-03-02 20:22:32 +0100

    smthng

--- server/utility/validateToken.js
+++ server/utility/validateToken.js
@@ -6,6 +6,8 @@ const validateToken = async(req, res, next) => {
    
     let token;
     let authHeader =  req.headers.Authorization || req.headers.authorization;  
+
+    console.log(authHeader);
     if(authHeader && authHeader.startsWith("Bearer")) { // The Authorization header starts with "Bearer"
         token = authHeader.split(" ")[1]; // The token is the second part of the header
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
@@ -13,7 +15,8 @@ const validateToken = async(req, res, next) => {
                 res.status(401);
                 throw new Error("User not Authorized!")
             }
-            req.user = decoded.user;       
+            req.userId = decoded.userId;  
+            console.log(req.userId)     
             next();
         }); 
     } else{ // If there is no token
