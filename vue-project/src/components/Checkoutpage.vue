b26d2681de2cb0a9f1ae0cb12dd715f7a223ce46
Sebpet <sebpet-9@student.ltu.se>
2025-03-03 19:20:52 +0100

    sadsadaa

--- vue-project/src/components/Checkoutpage.vue
+++ vue-project/src/components/Checkoutpage.vue
@@ -7,14 +7,21 @@
                 <tr>
                     <td id="td"> Product:{{ product.chocolate }}</td>
                     <td id="td"> Quantity:{{ product.Quantity }}</td>
+                    <td id="td"> Product price:{{ product.Price_single_product }}</td>
+                    <td id="td"> Total product price:{{ product.Price_products }}</td>
+                    <td id="td"> Product:{{ product.Size }}</td>
+                    <td id="td"> Image:{{ product.Image }}</td>
 
                 </tr>
                     <button>-</button>
                     <button>+</button>
+                    
             </tbody>
-            <div id="Buy">        
-                <button @click ="handleSubmit()" >Checkout</button>
+            <div id="Buy">
+                        <h2 >Total Price: {{ TotalPrice }}</h2>        
+                        <button @click ="handleSubmit()" >Checkout</button>
             </div>
+            
 
         </div>
 
@@ -32,23 +39,34 @@ import { onMounted, ref } from 'vue';
 import apiClient from '@/config/axios';
 
 const products = ref([])
+let TotalPrice = ref()
 
 const getCart = async () => {
     const response = await apiClient.get(`/product/getCart`);
-    console.log(response.data.data)
+    console.log(response.data)
+    console.log(response.data.total)
+    TotalPrice = response.data.total;
     for(let i = 0; i < response.data.data.length; i++ ) {
+        console.log(response.data.price[i])
         let product ={
-            id: response.data.data[i].User,
-            chocolate: response.data.data[i].Product,
-            Quantity: response.data.data[i].Quantity
+            chocolate: response.data.data[i].Description,
+            Quantity: response.data.data[i].Quantity,
+            Image: response.data.data[i].Image, 
+            Price_single_product: response.data.data[i].Price,
+            Price_products: response.data.price[i],
+            Size: response.data.data[i].Size
         }
-        console.log(product)
+        //console.log(product)
+       
         products.value.push(product);
     }
     //products = response.data;
     
 }
-//const handleSubmit = async ()
+const handleSubmit = async () => {
+    const response = await apiClient.post(`/product/checkOut`);
+    console.log(response)
+}
 onMounted(async () => {
     await getCart();
     
